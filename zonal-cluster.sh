467f69cd62ef1a681662179069773bcccf63ac4c
Himanshu Shekhar <43611225+Shekh1995@users.noreply.github.com>
2026-09-11 07:21:19 -0700

    Change machine type for cluster creation

--- zonal-cluster.sh
+++ zonal-cluster.sh
@@ -10,5 +10,5 @@ gcloud container clusters create $CLUSTER_NAME \
     --project=$PROJECT_ID \
     --zone=$ZONE \
     --num-nodes=1 \
-    --machine-type=e2-standard-4 \
+    --machine-type=c3d-highmem-16 \
     --release-channel=stable
