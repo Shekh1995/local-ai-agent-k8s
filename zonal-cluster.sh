138c5d4a3bdfe07d4b377be9ab13a2e4c73303e5
Himanshu Shekhar <43611225+Shekh1995@users.noreply.github.com>
2026-09-11 07:29:19 -0700

    Update zonal-cluster.sh

--- zonal-cluster.sh
+++ zonal-cluster.sh
@@ -3,12 +3,12 @@
 # Define targeted variables
 export PROJECT_ID=$(gcloud config get-value project)
 export CLUSTER_NAME=vllm-zonal-cluster
-export ZONE=europe-north1-a
+export ZONE=europe-west2-a
 
 # Create the GKE zonal cluster
 gcloud container clusters create $CLUSTER_NAME \
     --project=$PROJECT_ID \
     --zone=$ZONE \
     --num-nodes=1 \
-    --machine-type=c3d-standard-60 \
+    --machine-type=e2-highmem-16 \
     --release-channel=stable
