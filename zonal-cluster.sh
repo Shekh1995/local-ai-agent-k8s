78c3881d7e4dfb3c3bd51370d95f19df1742584b
Himanshu Shekhar <43611225+Shekh1995@users.noreply.github.com>
2026-09-11 07:25:32 -0700

    Update zonal-cluster.sh

--- zonal-cluster.sh
+++ zonal-cluster.sh
@@ -10,5 +10,5 @@ gcloud container clusters create $CLUSTER_NAME \
     --project=$PROJECT_ID \
     --zone=$ZONE \
     --num-nodes=1 \
-    --machine-type=c3d-highmem-16 \
+    --machine-type=c3d-standard-60 \
     --release-channel=stable
